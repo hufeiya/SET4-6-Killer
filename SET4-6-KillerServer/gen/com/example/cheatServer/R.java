df2a8f2bcb363151ae62a7008f2bfd3d7dbb02df
FelixCN <hufeiya0@gmail.com>
2014-05-23 20:01:01 +0800

    add vibrator function to XueZha mode

--- SET4-6-KillerServer/gen/com/example/cheatServer/R.java
+++ SET4-6-KillerServer/gen/com/example/cheatServer/R.java
@@ -14,20 +14,25 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f060006;
-        public static final int button2=0x7f060007;
-        public static final int editText3=0x7f060002;
-        public static final int name=0x7f060000;
-        public static final int scrollView1=0x7f060001;
-        public static final int send=0x7f060004;
-        public static final int sentence=0x7f060003;
-        public static final int textView1=0x7f060005;
-        public static final int textView2=0x7f060008;
-        public static final int textView3=0x7f060009;
+        public static final int button1=0x7f070006;
+        public static final int button2=0x7f070007;
+        public static final int editText3=0x7f070002;
+        public static final int group1=0x7f07000a;
+        public static final int name=0x7f070000;
+        public static final int scrollView1=0x7f070001;
+        public static final int send=0x7f070004;
+        public static final int sentence=0x7f070003;
+        public static final int shake=0x7f07000b;
+        public static final int textView1=0x7f070005;
+        public static final int textView2=0x7f070008;
+        public static final int textView3=0x7f070009;
     }
     public static final class layout {
         public static final int main=0x7f030000;
         public static final int welcome=0x7f030001;
+    }
+    public static final class menu {
+        public static final int clientmenu=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
--- SET4-6-KillerServer/src/com/example/cheatServer/ChatActivityClient.java
+++ SET4-6-KillerServer/src/com/example/cheatServer/ChatActivityClient.java
@@ -4,14 +4,20 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.os.Vibrator;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class ChatActivityClient extends Activity {
@@ -22,6 +28,9 @@ public class ChatActivityClient extends Activity {
 	 TextView show;
 	 Button send ;
 	 ScrollView scroll;
+	 Vibrator vibrator;//震动棒
+	 boolean shakingOn = false;
+	 long pattern[][] = {{100,100},{100,100,100,100},{100,100,100,100,100,100},{100,100,100,100,100,100,100,100}};//震动模式
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -31,6 +40,7 @@ public class ChatActivityClient extends Activity {
 		show = (TextView)findViewById(R.id.editText3);
 		send  = (Button)findViewById(R.id.send);
 		scroll = (ScrollView)findViewById(R.id.scrollView1);
+		vibrator = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
 		//4.0之后联网不能写入主线程内
 		Runnable  getTouch = new Runnable(){
 			@Override
@@ -64,15 +74,18 @@ public class ChatActivityClient extends Activity {
 				}
 			}
 		});
+		//接受网络线程传回的信息
 		handler = new Handler(){
 			public void handleMessage(Message msg){
 				super.handleMessage(msg);
-				String tmp = show.getText().toString();
-				show.setText(tmp + "\n\n" + msg.getData().getString("chat"));
+				String newMsg = msg.getData().getString("chat");
+				show.append("\n\n" + newMsg);
 				scrollToBottom(scroll,show);
+				shaking(newMsg);//发送震动
 			}
 		};
 	}
+	//网络线程，用户接收信息
 	class ChatThread extends Thread{
 		Socket s;
 		public ChatThread(Socket client) {
@@ -119,5 +132,59 @@ public class ChatActivityClient extends Activity {
 	protected void onResume() {
 		super.onResume();
 		scrollToBottom(scroll,show);
+	}
+	@Override//创建学渣端菜单
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.clientmenu, menu);
+		return true;
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.shake:
+			if( ! shakingOn)
+			{
+				shakingOn = true;
+				item.setChecked(true);
+			}
+			else
+			{
+				shakingOn = false;
+				item.setChecked(false);
+			}
+			break;
+
+		default:
+			break;
+		}
+		return true;
+	}
+	//答案震动提醒功能，A震动一次，B震动两次。。
+	public void shaking(String newMsg)
+	{
+		if( ! shakingOn)
+		{
+			return;
+		}
+		char key = newMsg.charAt(newMsg.length()-1);//最后一个字符即为答案
+		
+		switch (key) {
+		case 'A':
+			vibrator.vibrate(pattern[0], -1);
+			break;
+		case 'B':
+			vibrator.vibrate(pattern[1], -1);
+			break;
+		case 'C':
+			vibrator.vibrate(pattern[2], -1);
+			break;
+		case 'D':
+			vibrator.vibrate(pattern[3], -1);
+			break;
+
+		default:
+			break;
+		}
 	}
 }
